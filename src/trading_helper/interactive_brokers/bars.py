a870a1904e81d020299169b884aa1f66cdb39954
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2025-10-23 09:24:39 +0300

    Switch library to ib_insync and also add OHLC getting for a stock

--- src/trading_helper/interactive_brokers/bars.py
+++ src/trading_helper/interactive_brokers/bars.py
@@ -1,6 +1,29 @@
-from ib_async import IB, Stock, util
+from ib_insync import IB, Stock, util
 
 import pandas as pd
+
+
+def get_ohlc_for_stock(ticker: str, duration_string: str = '3 Y', bar_size: str = '1 day') -> pd.DataFrame:
+    ib = IB()
+    ib.connect('127.0.0.1', 7496, clientId=1)
+    stock = Stock(ticker, 'SMART', 'USD')
+    
+    # Request 1 year of daily historical data
+    bars = ib.reqHistoricalData(
+        stock, endDateTime='', durationStr=duration_string,
+        barSizeSetting=bar_size, whatToShow='TRADES', useRTH=True
+    )
+    df = util.df(bars)
+    ib.disconnect()
+    
+    if df.empty:
+        raise ValueError(f"Could not stock data for {ticker}")
+    
+    # Set datetime as index
+    df.set_index('date', inplace=True)
+    df.index = pd.to_datetime(df.index)
+    
+    return df
 
 def get_daily_candles_for_last_year(ib: IB, stock_contract: Stock) -> pd.DataFrame:
     """
